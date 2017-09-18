16d35ba186aac3f4701b4b403d303bb1f5692889
zengxuebing <zengxb94@foxmail.com>
2017-09-18 20:15:53 +0800

    fix format error

--- db.py
+++ db.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import sessionmaker
 import models
 
 if os.environ['PYTHON_ENV'] == 'production':
-  engine = create_engine(f'mysql+pymysql://${os.environ['DB_USER']}:{os.environ['DB_PASSPORT']}@127.0.0.1/eva_acfun?charset=utf8', encoding='utf8')
+  engine = create_engine(f'mysql+pymysql://{os.environ['DB_USER']}:{os.environ['DB_PASSPORT']}@127.0.0.1/eva_acfun?charset=utf8', encoding='utf8')
 else
   engine = create_engine('mysql+pymysql://root@127.0.0.1/eva_acfun_test?charset=utf8', encoding='utf8', echo = True)
 
