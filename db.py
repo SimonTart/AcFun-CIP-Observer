d984d4f39f3054c6af29b493e6e6cf208be20cb7
zengxuebing <zengxb94@foxmail.com>
2017-09-18 21:38:31 +0800

    down to python3.5

--- db.py
+++ db.py
@@ -6,7 +6,7 @@ import models
 if os.getenv('PYTHON_ENV', 'development') == 'production':
   user = os.getenv('DB_USER')
   password = os.getenv('DB_PASSWORD')
-  engine = create_engine(f'mysql+pymysql://{user}:{password}@127.0.0.1/eva_acfun?charset=utf8', encoding='utf8')
+  engine = create_engine('mysql+pymysql://{user}:{password}@127.0.0.1/eva_acfun?charset=utf8'.format(user=user,password=password), encoding='utf8')
 else:
   engine = create_engine('mysql+pymysql://root@127.0.0.1/eva_acfun_test?charset=utf8', encoding='utf8', echo = False)
 
