e620a6202e5aeffb94142130e3bdf985a1a15980
zengxuebing <zengxb94@foxmail.com>
2018-03-11 15:57:19 +0800

    添加视频区基本信息

--- getVideoSections.js
+++ getVideoSections.js
@@ -1,30 +1,4 @@
-import os
-
-ARTICLE_SECTIONS = ({
-  'type': '综合',
-  'channelId': 110,
-  'realmIds': '5,1,2,4',
-},{
-  'type': '工作·情感',
-  'channelId': 73,
-  'realmIds': '6,7',
-},{
-  'type': '动漫文化',
-  'channelId': 74,
-  'realmIds': '13,14',
-},
-{
-  'type': '漫画·轻小说',
-  'channelId': 75,
-  'realmIds': '15,16',
-},
-{
-  'type': '游戏',
-  'channelId': 164,
-  'realmIds': '8,10,11,9,12',
-})
-
-VIDEO_SECTIONS = [
+[
     {
         "name": "AC正义",
         "channelId": 178
@@ -338,18 +312,3 @@ VIDEO_SECTIONS = [
         ]
     }
 ]
-
-if os.getenv('PYTHON_ENV', 'development') == 'production':
-  db = {
-    'host': os.getenv('DB_HOST'),
-    'user': os.getenv('DB_USER'),
-    'password': os.getenv('DB_PASSPORT'),
-    'database': os.getenv('DB_NAME')
-  }
-else:
-  db = {
-    'host': '127.0.0.1',
-    'user': 'root',
-    'password': '',
-    'database': 'eva_acfun_dev'
-  }
