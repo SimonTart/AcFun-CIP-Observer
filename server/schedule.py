0974b09d020a62bac0ec1b84632b967d6ce68cbb
zengxuebing <zengxb94@foxmail.com>
2018-06-26 12:45:16 +0800

    优化抓取策略

--- server/schedule.py
+++ server/schedule.py
@@ -13,11 +13,11 @@ scheduler.add_job(contentSpider.crawlAllSectionsArticles, args= [articleSections
 # 抓取最新的视频
 scheduler.add_job(contentSpider.crawlAllSectionsVideos, args= [videoSections], name='抓取最新视频', trigger='interval', minutes=10)
 
-## 最近一天的文章的评论，每10分钟抓取一次最近的50条
-scheduler.add_job(commentSpider.crawlLatestComments, args= [1], name='抓取一天内最新评论', trigger='interval', minutes=10)
+## 最近一天的文章的评论，每15分钟抓取最近三天的评论
+scheduler.add_job(commentSpider.crawlLatestComments, args= [3], name='抓取一天内最新评论', trigger='interval', minutes=10)
 
 ## 每小时抓一次最近三天的评论
-scheduler.add_job(commentSpider.crawlLatestComments, args= [3], name='抓取三天内最新评论', trigger='interval', hours=1)
+# scheduler.add_job(commentSpider.crawlLatestComments, args= [3], name='抓取三天内最新评论', trigger='interval', hours=1)
 
 ## 最近一周的评论 每晚抓取一次评论
 scheduler.add_job(commentSpider.crawlLatestComments, kwargs= { 'day': 7, 'useThread': False, 'crawlAll': True}, name='抓取一周内文章评论', trigger='cron', hour=5, minute=30)
