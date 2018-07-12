3463c43e6aedc6ea498b34fd7875565173ca9969
zengxuebing <zengxb94@foxmail.com>
2018-07-12 13:36:02 +0800

    优化getProxy

--- server/proxy/proxy.py
+++ server/proxy/proxy.py
@@ -4,8 +4,11 @@ import logging
 from sentry import ravenClient
 
 def getProxy():
-    proxy = requests.get('http://127.0.0.1:5010/get').text
-    return proxy
+    try:
+        proxy = requests.get('http://127.0.0.1:5010/get').text
+        return proxy
+    except:
+        return getProxy()
 
 def deleteProxy(proxy):
     requests.get('http://127.0.0.1:5010/delete?proxy={}'.format(proxy))
@@ -23,9 +26,9 @@ def request(method, url, **kwargs):
         while requestCount > 0:
             hasException = False
             try:
-                res = requests.request(method, url, timeout = 10, proxies={'http': 'http://{}'.format(proxy)}, **kwargs)
-                res.json()
-                if res.status_code == 200 and res.json() is not None:
+                res = requests.request(method, url, timeout = 5, proxies={'http': 'http://{}'.format(proxy)}, **kwargs)
+                data = res.json().get('data')
+                if res.status_code == 200 and data is not None:
                     return res
                 else:
                     requestCount -= 1
