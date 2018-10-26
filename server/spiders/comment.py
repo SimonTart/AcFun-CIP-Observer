d1be57de206ae6da0b8c06da8a661051c543ab76
zengxuebing <zengxb94@foxmail.com>
2018-10-26 22:16:31 +0800

    评论抓取改造完成

--- server/spiders/comment.py
+++ server/spiders/comment.py
@@ -1,139 +1,117 @@
-import logging
-import sys
-from ..proxy import proxy
-import threading
+from ..proxy import Proxy
 import arrow
 from db import Session
 from ..models.comment import Comment
-from ..models.content import Content
-from time import time
-from sentry import ravenClient
 
-def requestComments(contentId, pageNumber = 1, pageSize = 50):
-    params = {
-        'isNeedAllCount': 'true',
-        'isReaderOnlyUpUser': 'false',
-        'isAscOrder': 'false',
-        'contentId': contentId,
-        'currentPage': pageNumber,
-        'pageSize': pageSize,
-    }
-    return proxy.get(
+
+class CommentSpider:
+    def __init__(
+        self,
+        content_id,
+        page_size=50,
+        crawl_all=None,
+        min_comment_time=None
+    ):
+        self.content_id = content_id
+        self.page_size = page_size
+        self.crawl_all = crawl_all
+        self.min_comment_time = min_comment_time
+
+    def request_comments(self, page_number):
+        params = {
+            'isNeedAllCount': 'true',
+            'isReaderOnlyUpUser': 'false',
+            'isAscOrder': 'false',
+            'contentId': self.content_id,
+            'currentPage': page_number,
+            'pageSize': self.page_size,
+        }
+        res = Proxy().request_acfun(
+            'get',
             'http://www.acfun.cn/comment_list_json.aspx',
-            params = params,
-            Referer = "http://www.acfun.cn/a/ac{}".format(contentId)
+            params=params,
+            Referer='http://www.acfun.cn/a/ac{}'.format(self.content_id)
         )
+        data = res.json().get('data')
+        return (data.get('commentContentArr'), data.get('commentList'), data.get('totalPage'))
 
-def getCommentDictFromRes(res):
-    data = res.json().get('data')
-    commentIdList = data.get('commentList')
-    return data.get('commentContentArr')
+    def get_comments_by_order(self):
+        comment_dict, comment_list, total_page = self.request_comments(page_number=1)
 
-def getCommentsByOrder(contentId, crawlAll):
-    """根据content ID抓取评论
-    Args:
-        contentId: content ID
-        crawlAll: 是否抓取此内容的所有评论, 如果是False, 那么只抓取前200个
+        # 只有一页直接返回
+        if total_page is 1:
+            return comment_dict.values()
 
-    Returns: commentList
-    """
-    res = requestComments(contentId)
-    if res.status_code != 200:
-        ravenClient.captureMessage('Comment Request Error', extra= { 'res': res, 'statusCode': res.status_code, 'text': res.text })
-        totalPage = 1
-        commentDict = {}
-    else:
-        totalPage = res.json().get('data').get('totalPage')
-        commentDict = getCommentDictFromRes(res)
+        # 如果要抓取全部的评论
+        if self.crawl_all is True:
+            for page_number in range(1, int(total_page)):
+                new_comment_dict, comment_list, _ = self.request_comments(page_number=page_number)
+                comment_dict.update(new_comment_dict)
+            return comment_dict.values()
 
-    if crawlAll is True:
-        for pageNumber in range(1, int(totalPage)):
-            newCommentDict = getCommentDictFromRes(requestComments(contentId, pageNumber + 1))
-            commentDict.update(newCommentDict)
+        # 如果要抓取某个时间范围内的评论
+        last_comment_id = comment_list[-1]
+        last_comment_date = comment_dict.get('c' + str(int(last_comment_id))).get('postDate')
+        if self.min_comment_time is not None:
+            # 第一页已经超过了，直接返回
+            if arrow.get(last_comment_date) < arrow.get(self.min_comment_time):
+                return comment_dict.values()
 
-    return commentDict.values()
+            # 继续遍历
+            for page_number in range(1, int(total_page)):
+                new_comment_dict, new_comment_list, _ = self.request_comments(page_number=page_number)
+                comment_dict.update(new_comment_dict)
 
+                last_comment_id = new_comment_list[-1]
+                last_comment_date = new_comment_dict.get('c' + str(int(last_comment_id))).get('postDate')
+                if arrow.get(last_comment_date) < arrow.get(self.min_comment_time):
+                    return comment_dict.values()
 
-def formatCommentToModel(comment, contentId):
-    return {
-        'id': comment.get('cid'),
-        'content': comment.get('content'),
-        'userId': comment.get('userID'),
-        'postDate': comment.get('postDate'),
-        'quoteId': comment.get('quoteId'),
-        'isDelete': comment.get('isDelete'),
-        'isUpDelete': comment.get('isUpDelete'),
-        'contentId': contentId,
-    }
+            return comment_dict.values()
 
-def fromatComments(comments, contentId):
-    return [formatCommentToModel(comment, contentId) for comment in comments]
+        raise Exception('获取评论出错')
 
-def saveComments(comments):
-    session = Session()
-    commentIds = { comment['id'] for comment in comments }
-    commentsInDB = session.query(Comment.id).filter(Comment.id.in_(commentIds)).all()
-    commentIdsInDB = { comment.id for comment in commentsInDB }
-    if commentIdsInDB is None:
-        commentIdsInDB = []
-    commentIdsInDB = set(commentIdsInDB)
-
-    # 添加新的评论
-    needAddCommentIds = commentIds - commentIdsInDB
-    needAddComments = list(filter(lambda c: c['id'] in needAddCommentIds, comments))
-    session.add_all([ Comment(**comment) for comment in needAddComments])
-    session.commit()
-
-    #更新旧的评论
-    needUpdateComments = list(filter(lambda c: c['id'] in commentIdsInDB and (c['isDelete'] is True or c['isUpDelete'] is True), comments))
-    for comment in needUpdateComments:
-        session.query(Comment).filter(Comment.id == comment.get('id')).update({
+    def format_comment_to_model(self, comment):
+        return {
+            'id': comment.get('cid'),
+            'content': comment.get('content'),
+            'userId': comment.get('userID'),
+            'postDate': comment.get('postDate'),
+            'quoteId': comment.get('quoteId'),
             'isDelete': comment.get('isDelete'),
-            'isUpDelete': comment.get('isUpDelete')
-        })
+            'isUpDelete': comment.get('isUpDelete'),
+            'contentId': self.content_id,
+        }
+
+    def save_comments(self, comments):
+        session = Session()
+        comment_ids = {comment['id'] for comment in comments}
+        comments_in_db = session.query(Comment.id).filter(Comment.id.in_(comment_ids)).all()
+        comment_ids_in_db = {comment.id for comment in comments_in_db}
+        if comment_ids_in_db is None:
+            comment_ids_in_db = []
+        comment_ids_in_db = set(comment_ids_in_db)
+
+        # 添加新的评论
+        need_add_comment_ids = comment_ids - comment_ids_in_db
+        need_add_comments = list(filter(lambda c: c['id'] in need_add_comment_ids, comments))
+        session.add_all([Comment(**comment) for comment in need_add_comments])
         session.commit()
 
-    session.close()
+        # 更新旧的评论
+        need_update_comments = list(
+            filter(lambda c: c['id'] in comment_ids_in_db and (c['isDelete'] is True or c['isUpDelete'] is True),
+                   comments))
+        for comment in need_update_comments:
+            session.query(Comment).filter(Comment.id == comment.get('id')).update({
+                'isDelete': comment.get('isDelete'),
+                'isUpDelete': comment.get('isUpDelete')
+            })
+            session.commit()
 
+        session.close()
 
-def crawlCommentsByContentId(contentId, crawlAll):
-    start  = time()
-    startGetTime = time()
-
-    comments = getCommentsByOrder(contentId, crawlAll)
-    timeOfGet = time() - startGetTime
-
-    startSaveTime = time()
-    comments = fromatComments(comments, contentId)
-    saveComments(comments)
-    timeOfSave = time() - startSaveTime
-
-    timeOfTotal = time() - start
-    # logging.info(
-    #     '抓取内容：' + str(contentId) + '评论' +
-    #     '[一共花费' + str(timeOfTotal) + ' 秒]' +
-    #     '[请求数据花费' + str(timeOfGet) +'秒]' +
-    #     '[处理并保存数据花费' + str(timeOfSave) +'秒]'
-    # )
-
-def crawlCommentsByContentIds(contentIds, crawlAll):
-    for contentId in contentIds:
-        crawlCommentsByContentId(contentId, crawlAll)
-
-def crawlLatestComments(day, useThread = True, threadCrawlNum = 30, crawlAll = False):
-    start = time()
-    session = Session()
-    contents = session.query(Content.id).filter(Content.publishedAt >= arrow.now().shift(days= -day).format('YYYY-MM-DD HH:MM:SS')).all()
-    contentIds = [ c.id for c in contents ]
-    if useThread:
-        threadList = []
-        for i in range(0, len(contentIds) + 1, threadCrawlNum):
-            t = threading.Thread(target = crawlCommentsByContentIds, args = (contentIds[i:i+threadCrawlNum], crawlAll))
-            t.start()
-            threadList.append(t)
-
-        for t in threadList:
-            t.join()
-    else:
-        crawlCommentsByContentIds(contentIds, crawlAll)
-    logging.info('此次一共抓取' + str(len(contentIds)) + '个内容的评论，共使用：' + str(time() - start) + '秒')
+    def crawl_comments(self):
+        comments = self.get_comments_by_order()
+        comments = [self.format_comment_to_model(comment) for comment in comments]
+        self.save_comments(comments)
--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import logging
 import traceback
 from server.spiders.content import ContentSpider
-import server.spiders.comment as commentSpider
+from server.spiders.comment import CommentSpider
 from server.schedule import scheduler
 from time import time
 from server import server
@@ -27,8 +27,13 @@ logging.basicConfig(
 # ContentSpider(min_published_date='2018-10-26 00:00:00').crawl_all_sections_articles(ARTICLE_SECTIONS)
 
 # 获取新评论的文章
-ContentSpider(article_order_type=1, min_latest_comment_time='2018-10-26 00:00:00').crawl_all_sections_articles(ARTICLE_SECTIONS)
+# ContentSpider(article_order_type=1, min_latest_comment_time='2018-10-26 00:00:00').crawl_all_sections_articles(ARTICLE_SECTIONS)
 
+# 获取content所有的评论
+# CommentSpider(content_id=4667805, crawl_all=True).crawl_comments()
+
+# 抓取时间范围内的评论
+# CommentSpider(content_id=4667805, min_comment_time='2018-10-26 12:00:00').crawl_comments()
 
 
 
