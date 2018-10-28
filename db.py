d52677be4d897fd0436c415680687181a9b0cfb3
zengxuebing <zengxb94@foxmail.com>
2018-10-28 11:32:53 +0800

    添加多线程抓取策略

--- db.py
+++ db.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import sessionmaker
 from config import db
 
 connect_url = 'mysql+pymysql://{user}:{password}@{host}/{database}?charset=utf8'.format(**db);
-engine = create_engine(connect_url, encoding='utf8')
+engine = create_engine(connect_url, encoding='utf8', pool_size=1024)
 
 Session = sessionmaker(bind=engine)
 connection = engine.connect()
--- server/schedule.py
+++ server/schedule.py
@@ -1,4 +1,5 @@
 import apscheduler
+import threading
 from apscheduler.schedulers.blocking import BlockingScheduler
 from config import ARTICLE_SECTIONS, VIDEO_SECTIONS, contentTypes
 from .spiders.content import ContentSpider
@@ -18,11 +19,35 @@ def crawl_section_content_latest_comment(sections, content_type):
             for sub_section in section['subSections']:
                 crawl_content_latest_comments(sub_section, content_type)
 
+def crawl_section_content_latest_comment_thread(sections, content_type):
+    thread_list = []
+    for section in sections:
+        if 'subSections' not in section:
+            t = threading.Thread(target=crawl_content_latest_comments, args=(section, content_type))
+            t.start()
+            thread_list.append(t)
+        else:
+            for sub_section in section['subSections']:
+                t = threading.Thread(target=crawl_content_latest_comments, args=(sub_section, content_type))
+                t.start()
+                thread_list.append(t)
+
+    return thread_list
 
 #抓取所有内容的最新评论
 def crawl_all_content_latest_comment():
     crawl_section_content_latest_comment(ARTICLE_SECTIONS, contentTypes['article'])
     crawl_section_content_latest_comment(VIDEO_SECTIONS, contentTypes['video'])
+
+def crawl_all_content_latest_comment_thread():
+    thread_list = []
+    article_thread_list = crawl_section_content_latest_comment_thread(ARTICLE_SECTIONS, contentTypes['article'])
+    video_thread_list = crawl_section_content_latest_comment_thread(VIDEO_SECTIONS, contentTypes['video'])
+    thread_list.extend(article_thread_list)
+    thread_list.extend(video_thread_list)
+
+    for thread in thread_list:
+        thread.join()
 
 
 def errorListener(event):
